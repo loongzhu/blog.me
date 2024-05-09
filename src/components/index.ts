36dc9083f1101f44eb95d6d3f613d37fb2ce1d14
Coder Poet <2332778528@qq.com>
2024-05-09 13:50:14 +0800

    chore: unified export components & layouts

---
+++ src/components/index.ts
@@ -0,0 +1,9 @@
+export { default as BaseHead } from "./BaseHead.astro";
+export { default as Footer } from "./Footer.astro";
+export { default as FormattedDate } from "./FormattedDate.astro";
+export { default as Header } from "./Header.astro";
+export { default as HeaderLink } from "./HeaderLink.astro";
+export { default as Icon } from "./Icon.astro";
+export { default as Signature } from "./Signature.astro";
+export { default as ThemeToggle } from "./ThemeToggle.astro";
+
