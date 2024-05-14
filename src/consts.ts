982edba4330d8aa6508e6d193307e945688160ca
Coder Poet <2332778528@qq.com>
2024-05-14 16:05:32 +0800

    chore: add mine consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
-export const MINE_EMAIL = 'mr.longzhentian@gmail.com';
+export const SITE_TITLE = "My Astro Blog";
+export const SITE_DESCRIPTION = "Welcome to my website!";
+export const MINE_NAME = "Long Zhentian";
+export const MINE_EMAIL = "mr.longzhentian@gmail.com";
