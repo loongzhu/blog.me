2f8eb15186d1ea46a05efda4390529e4931afcd5
Coder Poet <2332778528@qq.com>
2024-05-14 16:04:55 +0800

    chore: add fonts asstes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,17 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
+import { fileURLToPath, URL } from "url";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap(), UnoCSS()],
+  vite: {
+    resolve: {
+      alias: {
+        "@/fonts": fileURLToPath(new URL(`src/fonts`, import.meta.url)),
+      },
+    },
+  },
 });
