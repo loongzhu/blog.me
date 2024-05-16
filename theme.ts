9b2eaad406e9a1e96c1836a1b6b82b670676d44e
Coder Poet <2332778528@qq.com>
2024-05-16 16:54:08 +0800

    chore: unocss config theme

---
+++ theme.ts
@@ -0,0 +1,35 @@
+const light = {
+  colors: {
+    text: "#555555",
+    title: "#000000",
+    tip: "#60739f",
+    link: "#2337ff",
+    border: "#555555",
+    gray: "#cdcdcd",
+    hover: "#000000",
+    selection: "#dfdfdf",
+    activation: "#000000",
+    bg: "#f3f3f3",
+    "bg-reverse": "#050505",
+    "sb-thumb": "#919191",
+  },
+};
+
+const dark = {
+  colors: {
+    text: "#bbbbbb",
+    title: "#ffffff",
+    tip: "#60739f",
+    link: "#2337ff",
+    border: "#bbbbbb",
+    gray: "#3b3b3b",
+    hover: "#ffffff",
+    selection: "#282828",
+    activation: "#ffffff",
+    bg: "#050505",
+    "bg-reverse": "#f3f3f3",
+    "sb-thumb": "#555555",
+  },
+};
+
+export { dark, light };
--- uno.config.ts
+++ uno.config.ts
@@ -1,6 +1,41 @@
 // uno.config.ts
-import { defineConfig } from 'unocss'
+import {
+  defineConfig,
+  presetAttributify,
+  presetUno,
+  transformerDirectives,
+} from "unocss";
+
+import presetRemToPx from "@unocss/preset-rem-to-px";
+import presetTheme from "unocss-preset-theme";
+
+import { dark, light } from "./theme";
 
 export default defineConfig({
   // ...UnoCSS options
-})
+  // theme: light,
+  shortcuts: {
+    "flex-center": `flex justify-center items-center`,
+    "grid-center": `grid place-items-center`,
+
+    "transition-base": `transition transition-property-all transition-duration-300`,
+
+    "btn-base": `w-fit color-text bg-bg cursor-pointer border-rd-50 border-none op40 hover:op60 hover:bg-gray select-none transition-base`,
+    "btn-rect": `btn-base px-20 py-10 bg-gray op60 hover:op100`,
+    "btn-circle": `btn-base grid-center p-20`,
+  },
+
+  rules: [
+    // [/^w-([\.\d]+)$/, ([_, num]) => ({ width: `${num}px` })],
+  ],
+  presets: [
+    presetUno(),
+    presetTheme({ theme: { dark, light } }),
+    presetRemToPx({ baseFontSize: 4 }),
+    presetAttributify({
+      /* preset options */
+    }),
+  ],
+
+  transformers: [transformerDirectives()],
+});
