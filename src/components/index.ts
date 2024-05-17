cb84bc7c6c230e30dfd45feabfee57fe1acc64b8
Coder Poet <2332778528@qq.com>
2024-05-17 14:28:26 +0800

    fix: re-export the default import  error

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,15 @@
+/**
+ * re-export the default import
+ *
+ * An error occurred when you try to import the following components using an alias or index.ts in another component in the same folder.
+ * ` Cannot read properties of null (reading 'xxx')`
+ * But it will work when you change anything in the component and save it.
+ *
+ * And it works when you import the component directly from file.
+ *
+ * The error not be occurred when you import the component from another folder or built. Feel free to use it.
+ */
+
 export { default as BaseHead } from "./BaseHead.astro";
 export { default as Footer } from "./Footer.astro";
 export { default as FormattedDate } from "./FormattedDate.astro";
